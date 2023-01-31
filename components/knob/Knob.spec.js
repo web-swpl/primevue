0347ce535503079e67d208228249026ebafd8354
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-01-31 10:28:59 +0300

    Knob unit test completed

--- components/knob/Knob.spec.js
+++ components/knob/Knob.spec.js
@@ -17,31 +17,185 @@ describe('Knob.vue', () => {
         expect(wrapper.find('.p-knob-text').text()).toBe('20');
     });
 
-    it('should change with click event', async () => {
-        const event = { offsetX: 100, offsetY: 100 };
+    it('When onClick triggered', async () => {
+        await wrapper.find('.p-knob > svg').trigger('click', { offsetX: 100, offsetY: 100 });
 
-        await wrapper.vm.onClick(event);
-
-        expect(wrapper.emitted()['update:modelValue'][0]).toEqual([95]);
+        expect(wrapper.emitted('update:modelValue')).toBeTruthy();
     });
 
-    it('should min - max work', async () => {
-        await wrapper.setProps({ min: -50, max: 50 });
+    it('When onClick triggered and disabled props true', async () => {
+        await wrapper.setProps({ disabled: true });
+        await wrapper.find('.p-knob > svg').trigger('click', { offsetX: 100, offsetY: 100 });
 
-        const event = { offsetX: 100, offsetY: 100 };
-
-        await wrapper.vm.onClick(event);
-
-        expect(wrapper.emitted()['update:modelValue'][0]).toEqual([45]);
+        expect(wrapper.emitted('update:modelValue')).toBeFalsy();
     });
 
-    it('should step work', async () => {
-        await wrapper.setProps({ step: 10 });
+    it('When onClick triggered and readonly props true', async () => {
+        await wrapper.setProps({ readonly: true });
+        await wrapper.find('.p-knob > svg').trigger('click', { offsetX: 100, offsetY: 100 });
 
-        const event = { offsetX: 18, offsetY: 30 };
+        expect(wrapper.emitted('update:modelValue')).toBeFalsy();
+    });
 
-        await wrapper.vm.onClick(event);
+    it('When mousedown triggered', async () => {
+        global.window.addEventListener = vi.fn();
 
-        expect(wrapper.emitted()['update:modelValue'][0]).toEqual([30]);
+        await wrapper.find('.p-knob > svg').trigger('mousedown');
+
+        expect(window.addEventListener).toHaveBeenCalled();
+    });
+
+    it('When onClick triggered and disabled props true', async () => {
+        global.window.addEventListener = vi.fn();
+
+        await wrapper.setProps({ disabled: true });
+        await wrapper.find('.p-knob > svg').trigger('mousedown');
+
+        expect(window.addEventListener).not.toHaveBeenCalled();
+    });
+
+    it('When mouseup triggered', async () => {
+        global.window.removeEventListener = vi.fn();
+
+        await wrapper.find('.p-knob > svg').trigger('mouseup');
+
+        expect(window.removeEventListener).toHaveBeenCalled();
+    });
+
+    it('When mouseup triggered and disabled props true', async () => {
+        global.window.removeEventListener = vi.fn();
+
+        await wrapper.setProps({ disabled: true });
+        await wrapper.find('.p-knob > svg').trigger('mouseup');
+
+        expect(window.removeEventListener).not.toHaveBeenCalled();
+    });
+
+    it('When onTouchStart triggered', async () => {
+        global.window.addEventListener = vi.fn();
+
+        await wrapper.find('.p-knob > svg').trigger('touchstart');
+
+        expect(window.addEventListener).toHaveBeenCalled();
+    });
+
+    it('When onTouchStart triggered and disabled prop true', async () => {
+        global.window.addEventListener = vi.fn();
+
+        await wrapper.setProps({ disabled: true });
+        await wrapper.find('.p-knob > svg').trigger('touchstart');
+
+        expect(window.addEventListener).not.toHaveBeenCalled();
+    });
+
+    it('When onTouchEnd triggered', async () => {
+        global.window.removeEventListener = vi.fn();
+
+        await wrapper.find('.p-knob > svg').trigger('touchend');
+
+        expect(window.removeEventListener).toHaveBeenCalled();
+    });
+
+    it('When onTouchEnd triggered and disabled props true', async () => {
+        global.window.removeEventListener = vi.fn();
+
+        await wrapper.setProps({ disabled: true });
+        await wrapper.find('.p-knob > svg').trigger('touchend');
+
+        expect(window.removeEventListener).not.toHaveBeenCalled();
+    });
+
+    it('When onMouseMove triggered', async () => {
+        const updateValueSpy = vi.spyOn(wrapper.vm, 'updateValue');
+
+        wrapper.vm.onMouseMove({ offsetX: 100, offsetY: 100, preventDefault: () => {} });
+
+        expect(updateValueSpy).toHaveBeenCalled();
+    });
+
+    it('When onMouseMove triggered and disabled true', async () => {
+        await wrapper.setProps({ disabled: true });
+        const updateValueSpy = vi.spyOn(wrapper.vm, 'updateValue');
+
+        wrapper.vm.onMouseMove({ offsetX: 100, offsetY: 100, preventDefault: () => {} });
+
+        expect(updateValueSpy).not.toHaveBeenCalled();
+    });
+
+    describe('keyDown events', () => {
+        it('When ArrowRight triggered', async () => {
+            const updateModelValueSpy = vi.spyOn(wrapper.vm, 'updateModelValue');
+
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'ArrowRight' });
+
+            expect(updateModelValueSpy).toHaveBeenCalled();
+        });
+
+        it('When ArrowUp triggered', async () => {
+            const updateModelValueSpy = vi.spyOn(wrapper.vm, 'updateModelValue');
+
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'ArrowUp' });
+
+            expect(updateModelValueSpy).toHaveBeenCalled();
+        });
+
+        it('When ArrowLeft triggered', async () => {
+            const updateModelValueSpy = vi.spyOn(wrapper.vm, 'updateModelValue');
+
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'ArrowLeft' });
+
+            expect(updateModelValueSpy).toHaveBeenCalled();
+        });
+
+        it('When ArrowDown triggered', async () => {
+            const updateModelValueSpy = vi.spyOn(wrapper.vm, 'updateModelValue');
+
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'ArrowDown' });
+
+            expect(updateModelValueSpy).toHaveBeenCalled();
+        });
+
+        it('When PageUp triggered', async () => {
+            const updateModelValueSpy = vi.spyOn(wrapper.vm, 'updateModelValue');
+
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'PageUp' });
+
+            expect(updateModelValueSpy).toHaveBeenCalled();
+        });
+
+        it('When PageDown triggered', async () => {
+            const updateModelValueSpy = vi.spyOn(wrapper.vm, 'updateModelValue');
+
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'PageDown' });
+
+            expect(updateModelValueSpy).toHaveBeenCalled();
+        });
+
+        it('When Home triggered', async () => {
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'Home' });
+
+            expect(wrapper.emitted('update:modelValue')).toBeTruthy();
+        });
+
+        it('When End triggered', async () => {
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'End' });
+
+            expect(wrapper.emitted('update:modelValue')).toBeTruthy();
+        });
+
+        it('When End triggered and disabled false', async () => {
+            await wrapper.setProps({ disabled: true });
+            await wrapper.find('.p-knob > svg').trigger('keydown', { code: 'End' });
+
+            expect(wrapper.emitted('update:modelValue')).toBeFalsy();
+        });
+    });
+
+    it('When min max properties bigger than 0 zeroRadians property should be return mapRange', async () => {
+        const mapRangeSpy = vi.spyOn(wrapper.vm, 'mapRange');
+
+        await wrapper.setProps({ min: 10, max: 20 });
+
+        expect(mapRangeSpy).toHaveBeenCalled();
     });
 });
