670cfb241e75413803a77bff5f091bfad2ca71fe
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-01-31 16:52:59 +0300

    footer updates

--- layouts/AppFooter.vue
+++ layouts/AppFooter.vue
@@ -1,20 +1,8 @@
 <template>
     <div class="layout-footer">
-        <div class="layout-footer-left">
+        <div>
             <span>PrimeVue 3.23.0 on Vue 3 by </span>
-            <a href="https://www.primetek.com.tr">PrimeTek</a>
-        </div>
-
-        <div class="layout-footer-right">
-            <a href="https://github.com/primefaces/primevue" class="mr-3">
-                <i class="pi pi-github"></i>
-            </a>
-            <a href="https://twitter.com/primevue" class="mr-3">
-                <i class="pi pi-twitter"></i>
-            </a>
-            <a href="https://discord.com/invite/gzKFYnpmCY">
-                <i class="pi pi-discord"></i>
-            </a>
+            <a href="https://www.primetek.com.tr" target="_blank" rel="noopener noreferrer">PrimeTek</a>
         </div>
     </div>
 </template>
