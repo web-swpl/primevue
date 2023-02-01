53b92169d38f4b6f15e1a51bb1df4f0a35fa16b4
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-01 16:56:21 +0300

    Inplace showcase updated

---
+++ doc/inplace/ImageDoc.vue
@@ -0,0 +1,73 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Any content such as an image can be placed inside an Inplace.</p>
+    </DocSectionText>
+    <div class="card">
+        <Inplace>
+            <template #display>
+                <span class="pi pi-search" style="vertical-align: middle"></span>
+                <span style="margin-left: 0.5rem; vertical-align: middle">View Picture</span>
+            </template>
+            <template #content>
+                <img alt="Nature" src="/demo/images/nature/nature1.jpg" />
+            </template>
+        </Inplace>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+<Inplace>
+    <template #display>
+        <span class="pi pi-search" style="vertical-align: middle"></span>
+        <span style="margin-left: 0.5rem; vertical-align: middle">View Picture</span>
+    </template>
+    <template #content>
+        <img alt="Nature" src="/demo/images/nature/nature1.jpg" />
+    </template>
+</Inplace>`,
+                options: `
+<template>
+    <div class="card">
+        <Inplace>
+            <template #display>
+                <span class="pi pi-search" style="vertical-align: middle"></span>
+                <span style="margin-left: 0.5rem; vertical-align: middle">View Picture</span>
+            </template>
+            <template #content>
+                <img c src="https://primereact.org/images/nature/nature1.jpg"/>
+            </template>
+        </Inplace>
+    </div>
+</template>
+<script>
+
+<\/script>`,
+                composition: `
+<template>
+    <div class="card">
+        <Inplace>
+            <template #display>
+                <span class="pi pi-search" style="vertical-align: middle"></span>
+                <span style="margin-left: 0.5rem; vertical-align: middle">View Picture</span>
+            </template>
+            <template #content>
+                <img alt="Nature" src="https://primereact.org/images/nature/nature1.jpg" />
+            </template>
+        </Inplace>
+    </div>
+</template>
+
+<script setup>
+
+<\/script>`
+            }
+        };
+    }
+};
+</script>
