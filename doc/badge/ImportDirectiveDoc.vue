a1fc66f0d4ef7a92f4128966f085cbe090d51ede
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-01 11:38:43 +0300

    Badge showcase updated

---
+++ doc/badge/ImportDirectiveDoc.vue
@@ -0,0 +1,20 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>When used as a directive, badge needs to be configured at the application with a name of your choice.</p>
+    </DocSectionText>
+    <DocSectionCode :code="code" hideToggleCode import hideCodeSandbox hideStackBlitz />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `import BadgeDirective from 'primevue/badgedirective';
+
+app.directive('badge', BadgeDirective);`
+            }
+        };
+    }
+};
+</script>
