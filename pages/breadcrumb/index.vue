04967102412d13ecd74951d69ba1cd5174c73a3d
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-02 14:06:52 +0300

    Breadcrumb showcase updated

--- pages/breadcrumb/index.vue
+++ pages/breadcrumb/index.vue
@@ -5,36 +5,64 @@
             <Meta name="description" content="Breadcrumb provides contextual information about page hierarchy." />
         </Head>
 
-        <div class="content-section introduction">
-            <div class="feature-intro">
-                <h1>Breadcrumb</h1>
-                <p>Breadcrumb provides contextual information about page hierarchy.</p>
+        <div class="doc">
+            <div class="doc-main">
+                <div class="doc-intro">
+                    <h1>Breadcrumb</h1>
+                    <p>Breadcrumb provides contextual information about page hierarchy.</p>
+                </div>
+                <DocSections :docs="docs" />
             </div>
-            <AppDemoActions />
+            <DocSectionNav :docs="docs" />
         </div>
-
-        <div class="content-section implementation">
-            <div class="card">
-                <Breadcrumb :home="home" :model="items" aria-label="breadcrumb" />
-            </div>
-        </div>
-
-        <BreadcrumbDoc />
     </div>
 </template>
 
 <script>
-import BreadcrumbDoc from './BreadcrumbDoc';
+import AccessibilityDoc from '../../doc/breadcrumb/AccessibilityDoc';
+import BasicDoc from '../../doc/breadcrumb/BasicDoc';
+import ImportDoc from '../../doc/breadcrumb/ImportDoc';
+import StyleDoc from '../../doc/breadcrumb/StyleDoc';
+import TemplateDoc from '../../doc/breadcrumb/TemplateDoc';
 
 export default {
     data() {
         return {
             home: { icon: 'pi pi-home', to: '/' },
-            items: [{ label: 'Computer' }, { label: 'Notebook' }, { label: 'Accessories' }, { label: 'Backpacks' }, { label: 'Item', to: '/breadcrumb' }]
+            items: [{ label: 'Computer' }, { label: 'Notebook' }, { label: 'Accessories' }, { label: 'Backpacks' }, { label: 'Item', to: '/breadcrumb' }],
+            docs: [
+                {
+                    id: 'import',
+                    label: 'Import',
+                    component: ImportDoc
+                },
+                {
+                    id: 'basic',
+                    label: 'Basic',
+                    component: BasicDoc
+                },
+                {
+                    id: 'template',
+                    label: 'Template',
+                    component: TemplateDoc
+                },
+                {
+                    id: 'style',
+                    label: 'Style',
+                    component: StyleDoc
+                },
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility',
+                    component: AccessibilityDoc
+                },
+                {
+                    id: 'api',
+                    label: 'API',
+                    doc: [{ name: 'Breadcrumb', pathname: '/modules/breadcrumb.html' }]
+                }
+            ]
         };
-    },
-    components: {
-        BreadcrumbDoc: BreadcrumbDoc
     }
 };
 </script>
