3ea04833b2472768b22b4b52ccf9ef2be153526d
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-03 11:11:15 +0300

    ContextMenu showcase updated

--- pages/contextmenu/index.vue
+++ pages/contextmenu/index.vue
@@ -5,169 +5,65 @@
             <Meta name="description" content="ContextMenu displays an overlay menu on right click of its target." />
         </Head>
 
-        <div class="content-section introduction">
-            <div class="feature-intro">
-                <h1>ContextMenu</h1>
-                <p>
-                    ContextMenu displays an overlay menu on right click of its target. Note that components like DataTable has special integration with ContextMenu. Refer to documentation of the individual documentation of the with context menu
-                    support.
-                </p>
+        <div class="doc">
+            <div class="doc-main">
+                <div class="doc-intro">
+                    <h1>ContextMenu</h1>
+                    <p>
+                        ContextMenu displays an overlay menu on right click of its target. Note that components like DataTable has special integration with ContextMenu. Refer to documentation of the individual documentation of the components having a
+                        special integration.
+                    </p>
+                </div>
+                <DocSections :docs="docs" />
             </div>
-            <AppDemoActions />
+            <DocSectionNav :docs="docs" />
         </div>
-
-        <div class="content-section implementation">
-            <div class="card">
-                <img alt="logo" src="/images/nature/nature3.jpg" @contextmenu="onImageRightClick" aria-haspopup="true" />
-                <ContextMenu ref="menu" :model="items" />
-            </div>
-        </div>
-
-        <ContextMenuDoc />
     </div>
 </template>
 
 <script>
-import ContextMenuDoc from './ContextMenuDoc';
+import AccessibilityDoc from '../../doc/contextmenu/AccessibilityDoc';
+import BasicDoc from '../../doc/contextmenu/BasicDoc';
+import DocumentDoc from '../../doc/contextmenu/DocumentDoc';
+import ImportDoc from '../../doc/contextmenu/ImportDoc';
+import StyleDoc from '../../doc/contextmenu/StyleDoc';
 
 export default {
     data() {
         return {
-            items: [
+            docs: [
                 {
-                    label: 'File',
-                    icon: 'pi pi-fw pi-file',
-                    items: [
-                        {
-                            label: 'New',
-                            icon: 'pi pi-fw pi-plus',
-                            items: [
-                                {
-                                    label: 'Bookmark',
-                                    icon: 'pi pi-fw pi-bookmark'
-                                },
-                                {
-                                    label: 'Video',
-                                    icon: 'pi pi-fw pi-video'
-                                }
-                            ]
-                        },
-                        {
-                            label: 'Delete',
-                            icon: 'pi pi-fw pi-trash'
-                        },
-                        {
-                            separator: true
-                        },
-                        {
-                            label: 'Export',
-                            icon: 'pi pi-fw pi-external-link'
-                        }
-                    ]
+                    id: 'import',
+                    label: 'Import',
+                    component: ImportDoc
                 },
                 {
-                    label: 'Edit',
-                    icon: 'pi pi-fw pi-pencil',
-                    items: [
-                        {
-                            label: 'Left',
-                            icon: 'pi pi-fw pi-align-left'
-                        },
-                        {
-                            label: 'Right',
-                            icon: 'pi pi-fw pi-align-right'
-                        },
-                        {
-                            label: 'Center',
-                            icon: 'pi pi-fw pi-align-center'
-                        },
-                        {
-                            label: 'Justify',
-                            icon: 'pi pi-fw pi-align-justify'
-                        }
-                    ]
+                    id: 'basic',
+                    label: 'Basic',
+                    component: BasicDoc
                 },
                 {
-                    label: 'Users',
-                    icon: 'pi pi-fw pi-user',
-                    items: [
-                        {
-                            label: 'New',
-                            icon: 'pi pi-fw pi-user-plus'
-                        },
-                        {
-                            label: 'Delete',
-                            icon: 'pi pi-fw pi-user-minus'
-                        },
-                        {
-                            label: 'Search',
-                            icon: 'pi pi-fw pi-users',
-                            items: [
-                                {
-                                    label: 'Filter',
-                                    icon: 'pi pi-fw pi-filter',
-                                    items: [
-                                        {
-                                            label: 'Print',
-                                            icon: 'pi pi-fw pi-print'
-                                        }
-                                    ]
-                                },
-                                {
-                                    icon: 'pi pi-fw pi-bars',
-                                    label: 'List'
-                                }
-                            ]
-                        }
-                    ]
+                    id: 'document',
+                    label: 'Document',
+                    component: DocumentDoc
                 },
                 {
-                    label: 'Events',
-                    icon: 'pi pi-fw pi-calendar',
-                    items: [
-                        {
-                            label: 'Edit',
-                            icon: 'pi pi-fw pi-pencil',
-                            items: [
-                                {
-                                    label: 'Save',
-                                    icon: 'pi pi-fw pi-calendar-plus'
-                                },
-                                {
-                                    label: 'Delete',
-                                    icon: 'pi pi-fw pi-calendar-minus'
-                                }
-                            ]
-                        },
-                        {
-                            label: 'Archieve',
-                            icon: 'pi pi-fw pi-calendar-times',
-                            items: [
-                                {
-                                    label: 'Remove',
-                                    icon: 'pi pi-fw pi-calendar-minus'
-                                }
-                            ]
-                        }
-                    ]
+                    id: 'style',
+                    label: 'Style',
+                    component: StyleDoc
                 },
                 {
-                    separator: true
+                    id: 'accessibility',
+                    label: 'Accessibility',
+                    component: AccessibilityDoc
                 },
                 {
-                    label: 'Quit',
-                    icon: 'pi pi-fw pi-power-off'
+                    id: 'api',
+                    label: 'API',
+                    doc: [{ name: 'ContextMenu', pathname: '/modules/contextmenu.html' }]
                 }
             ]
         };
-    },
-    methods: {
-        onImageRightClick(event) {
-            this.$refs.menu.show(event);
-        }
-    },
-    components: {
-        ContextMenuDoc: ContextMenuDoc
     }
 };
 </script>
