9f21e35ce9c688ebf0b2eb0499e3f028833fa5f1
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-02-06 01:44:25 +0300

    ScrollPanel completed

--- pages/scrollpanel/index.vue
+++ pages/scrollpanel/index.vue
@@ -5,68 +5,66 @@
             <Meta name="description" content="ScrollPanel is a cross browser, lightweight and skinnable alternative to native browser scrollbar." />
         </Head>
 
-        <div class="content-section introduction">
-            <div class="feature-intro">
-                <h1>ScrollPanel</h1>
-                <p>ScrollPanel is a cross browser, lightweight and themable alternative to native browser scrollbar.</p>
-            </div>
-            <AppDemoActions />
-        </div>
-
-        <div class="content-section implementation">
-            <div class="card">
-                <div class="grid">
-                    <div class="col-12 md:col-4 p-5">
-                        <ScrollPanel style="width: 100%; height: 200px">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae et leo duis ut diam. Ultricies mi quis hendrerit dolor magna eget est lorem. Amet
-                                consectetur adipiscing elit ut. Nam libero justo laoreet sit amet. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Est ultricies integer quis auctor elit sed vulputate. Consequat ac felis donec et. Tellus
-                                orci ac auctor augue mauris. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Tincidunt arcu non sodales neque sodales. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.
-                                Sodales ut etiam sit amet nisl purus. Cursus sit amet dictum sit amet. Tristique senectus et netus et malesuada fames ac turpis egestas. Et tortor consequat id porta nibh venenatis cras sed. Diam maecenas ultricies mi
-                                eget mauris. Eget egestas purus viverra accumsan in nisl nisi. Suscipit adipiscing bibendum est ultricies integer. Mattis aliquam faucibus purus in massa tempor nec.
-                            </p>
-                        </ScrollPanel>
-                    </div>
-                    <div class="col-12 md:col-4 p-5">
-                        <ScrollPanel style="width: 100%; height: 200px" class="custombar1">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae et leo duis ut diam. Ultricies mi quis hendrerit dolor magna eget est lorem. Amet
-                                consectetur adipiscing elit ut. Nam libero justo laoreet sit amet. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Est ultricies integer quis auctor elit sed vulputate. Consequat ac felis donec et. Tellus
-                                orci ac auctor augue mauris. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Tincidunt arcu non sodales neque sodales. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.
-                                Sodales ut etiam sit amet nisl purus. Cursus sit amet dictum sit amet. Tristique senectus et netus et malesuada fames ac turpis egestas. Et tortor consequat id porta nibh venenatis cras sed. Diam maecenas ultricies mi
-                                eget mauris. Eget egestas purus viverra accumsan in nisl nisi. Suscipit adipiscing bibendum est ultricies integer. Mattis aliquam faucibus purus in massa tempor nec.
-                            </p>
-                        </ScrollPanel>
-                    </div>
-                    <div class="col-12 md:col-4 p-5">
-                        <ScrollPanel style="width: 100%; height: 200px" class="custombar2">
-                            <p style="width: 600px">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae et leo duis ut diam. Ultricies mi quis hendrerit dolor magna eget est lorem. Amet
-                                consectetur adipiscing elit ut. Nam libero justo laoreet sit amet. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Est ultricies integer quis auctor elit sed vulputate. Consequat ac felis donec et. Tellus
-                                orci ac auctor augue mauris. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Tincidunt arcu non sodales neque sodales. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.
-                                Sodales ut etiam sit amet nisl purus. Cursus sit amet dictum sit amet. Tristique senectus et netus et malesuada fames ac turpis egestas. Et tortor consequat id porta nibh venenatis cras sed. Diam maecenas ultricies mi
-                                eget mauris. Eget egestas purus viverra accumsan in nisl nisi. Suscipit adipiscing bibendum est ultricies integer. Mattis aliquam faucibus purus in massa tempor nec.
-                            </p>
-                        </ScrollPanel>
-                    </div>
+        <div class="doc">
+            <div class="doc-main">
+                <div class="doc-intro">
+                    <h1>ScrollPanel</h1>
+                    <p>ScrollPanel is a cross browser, lightweight and themable alternative to native browser scrollbar.</p>
                 </div>
+                <DocSections :docs="docs" />
             </div>
+            <DocSectionNav :docs="docs" />
         </div>
-
-        <ScrollPanelDoc />
     </div>
 </template>
 <script>
-import ScrollPanelDoc from './ScrollPanelDoc';
+import AccessibilityDoc from '/doc/scrollpanel/AccessibilityDoc';
+import BasicDoc from '/doc/scrollpanel/BasicDoc';
+import CustomDoc from '/doc/scrollpanel/CustomDoc';
+import ImportDoc from '/doc/scrollpanel/ImportDoc';
+import StyleDoc from '/doc/scrollpanel/StyleDoc';
 
 export default {
-    components: {
-        ScrollPanelDoc: ScrollPanelDoc
+    data() {
+        return {
+            docs: [
+                {
+                    id: 'import',
+                    label: 'Import',
+                    component: ImportDoc
+                },
+                {
+                    id: 'basic',
+                    label: 'Basic',
+                    component: BasicDoc
+                },
+                {
+                    id: 'custom',
+                    label: 'Custom',
+                    component: CustomDoc
+                },
+                {
+                    id: 'style',
+                    label: 'Style',
+                    component: StyleDoc
+                },
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility',
+                    component: AccessibilityDoc
+                },
+                {
+                    id: 'api',
+                    label: 'API',
+                    doc: [{ name: 'ScrollPanel', pathname: '/modules/scrollpanel.html' }]
+                }
+            ]
+        };
     }
 };
 </script>
 
-<style lang="scss" scoped>
+<!-- <style lang="scss" scoped>
 ::v-deep(.p-scrollpanel) {
     p {
         padding: 0.5rem;
@@ -104,4 +102,4 @@ export default {
         }
     }
 }
-</style>
+</style> -->
