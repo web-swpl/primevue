fb13bdf37669ed8b5889e904e2eecb49cd4a797d
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-08 12:00:33 +0300

    MultiSelect showcase updated

---
+++ doc/multiselect/DisabledDoc.vue
@@ -0,0 +1,46 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>
+            MultiSelect is used as a controlled component with <i>v-model</i> property along with an <i>options</i> collection. Label and value of an option are defined with the <i>optionLabel</i> and <i>optionValue</i> properties respectively.
+            Default property name for the <i>optionLabel</i> is <i>label</i> and <i>value</i> for the <i>optionValue</i>. If <i>optionValue</i> is omitted and the object has no <i>value</i> property, the object itself becomes the value of an option.
+            Note that, when options are simple primitive values such as a string array, no <i>optionLabel</i> and <i>optionValue</i> would be necessary.
+        </p>
+    </DocSectionText>
+    <div class="flex justify-content-center">
+        <MultiSelect disabled placeholder="Select Cities" class="w-full md:w-20rem" />
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+<MultiSelect disabled placeholder="Select Cities" class="w-full md:w-20rem" />`,
+                options: `
+<template>
+    <div class="flex justify-content-center">
+        <MultiSelect disabled placeholder="Select Cities" class="w-full md:w-20rem" />
+    </div>
+</template>
+
+<script>
+
+<\/script>`,
+                composition: `
+<template>
+    <div class="flex justify-content-center">
+        <MultiSelect disabled placeholder="Select Cities" class="w-full md:w-20rem" />
+    </div>
+</template>
+
+<script setup>
+
+<\/script>`
+            }
+        };
+    }
+};
+</script>
---
+++ doc/multiselect/TemplateDoc.vue
@@ -0,0 +1,135 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Available options and the selected options support templating with <i>option</i> and <i>value</i> properties respectively. In addition, header, footer, filter, optiongroup, chip and indicator sections can be templated as well.</p>
+    </DocSectionText>
+    <div class="card flex justify-content-center">
+        <MultiSelect v-model="selectedCountries" :options="countries" optionLabel="name" placeholder="Select Countries" display="chip" class="w-full md:w-20rem">
+            <template #option="slotProps">
+                <div class="flex align-items-center">
+                    <img :alt="slotProps.option.name" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`flag flag-${slotProps.option.code.toLowerCase()} mr-2`" style="width: 18px" />
+                    <div>{{ slotProps.option.name }}</div>
+                </div>
+            </template>
+            <template #footer>
+                <div class="py-2 px-3">
+                    <b>{{ selectedCountries ? selectedCountries.length : 0 }}</b> item{{ (selectedCountries ? selectedCountries.length : 0) > 1 ? 's' : '' }} selected.
+                </div>
+            </template>
+        </MultiSelect>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedCountries: null,
+            countries: [
+                { name: 'Australia', code: 'AU' },
+                { name: 'Brazil', code: 'BR' },
+                { name: 'China', code: 'CN' },
+                { name: 'Egypt', code: 'EG' },
+                { name: 'France', code: 'FR' },
+                { name: 'Germany', code: 'DE' },
+                { name: 'India', code: 'IN' },
+                { name: 'Japan', code: 'JP' },
+                { name: 'Spain', code: 'ES' },
+                { name: 'United States', code: 'US' }
+            ],
+            code: {
+                basic: `
+<MultiSelect v-model="selectedCountries" :options="countries" optionLabel="name" placeholder="Select Countries" display="chip" class="w-full md:w-20rem">
+    <template #option="slotProps">
+        <div class="flex align-items-center">
+            <img :alt="slotProps.option.name" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="\`flag flag-\${slotProps.option.code.toLowerCase()} mr-2\`" style="width: 18px" />
+            <div>{{ slotProps.option.name }}</div>
+        </div>
+    </template>
+    <template #footer>
+        <div class="py-2 px-3">
+            <b>{{ selectedCountries ? selectedCountries.length : 0 }}</b> item{{ (selectedCountries ? selectedCountries.length : 0) > 1 ? 's' : '' }} selected.
+        </div>
+    </template>
+</MultiSelect>`,
+                options: `
+<template>
+    <div class="card flex justify-content-center">
+        <MultiSelect v-model="selectedCountries" :options="countries" optionLabel="name" placeholder="Select Countries" display="chip" class="w-full md:w-20rem">
+            <template #option="slotProps">
+                <div class="flex align-items-center">
+                    <img :alt="slotProps.option.name" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="\`flag flag-\${slotProps.option.code.toLowerCase()} mr-2\`" style="width: 18px" />
+                    <div>{{ slotProps.option.name }}</div>
+                </div>
+            </template>
+            <template #footer>
+                <div class="py-2 px-3">
+                    <b>{{ selectedCountries ? selectedCountries.length : 0 }}</b> item{{ (selectedCountries ? selectedCountries.length : 0) > 1 ? 's' : '' }} selected.
+                </div>
+            </template>
+        </MultiSelect>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedCountries: null,
+            countries: [
+                { name: 'Australia', code: 'AU' },
+                { name: 'Brazil', code: 'BR' },
+                { name: 'China', code: 'CN' },
+                { name: 'Egypt', code: 'EG' },
+                { name: 'France', code: 'FR' },
+                { name: 'Germany', code: 'DE' },
+                { name: 'India', code: 'IN' },
+                { name: 'Japan', code: 'JP' },
+                { name: 'Spain', code: 'ES' },
+                { name: 'United States', code: 'US' }
+            ]
+        };
+    }
+};
+<\/script>`,
+                composition: `
+<template>
+    <div class="card flex justify-content-center">
+        <MultiSelect v-model="selectedCountries" :options="countries" optionLabel="name" placeholder="Select Countries" display="chip" class="w-full md:w-20rem">
+            <template #option="slotProps">
+                <div class="flex align-items-center">
+                    <img :alt="slotProps.option.name" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="\`flag flag-\${slotProps.option.code.toLowerCase()} mr-2\`" style="width: 18px" />
+                    <div>{{ slotProps.option.name }}</div>
+                </div>
+            </template>
+            <template #footer>
+                <div class="py-2 px-3">
+                    <b>{{ selectedCountries ? selectedCountries.length : 0 }}</b> item{{ (selectedCountries ? selectedCountries.length : 0) > 1 ? 's' : '' }} selected.
+                </div>
+            </template>
+        </MultiSelect>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const selectedCountries = ref();
+const countries = ref([
+    { name: 'Australia', code: 'AU' },
+    { name: 'Brazil', code: 'BR' },
+    { name: 'China', code: 'CN' },
+    { name: 'Egypt', code: 'EG' },
+    { name: 'France', code: 'FR' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'India', code: 'IN' },
+    { name: 'Japan', code: 'JP' },
+    { name: 'Spain', code: 'ES' },
+    { name: 'United States', code: 'US' }
+]);
+<\/script>`
+            }
+        };
+    }
+};
+</script>
