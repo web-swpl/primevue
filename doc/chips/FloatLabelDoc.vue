2945963ae3480ee0f6dd19d21088f5b16bd1bdce
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-09 12:40:37 +0300

    Chips showcase updated

---
+++ doc/chips/FloatLabelDoc.vue
@@ -0,0 +1,63 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Chips is used as a controlled input with <i>v-model</i> property where it should be an array.</p>
+    </DocSectionText>
+    <div class="card p-fluid">
+        <span class="p-float-label">
+            <Chips id="chips" v-model="value" />
+            <label for="chips">Chips</label>
+        </span>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: null,
+            code: {
+                basic: `
+<span class="p-float-label">
+    <Chips id="chips" v-model="value" />
+    <label for="chips">Chips</label>
+</span>`,
+                options: `
+<template>
+    <div class="card p-fluid">
+        <span class="p-float-label">
+            <Chips id="chips" v-model="value" />
+            <label for="chips">Chips</label>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: null
+        };
+    }
+};
+<\/script>`,
+                composition: `
+<template>
+    <div class="card p-fluid">
+        <span class="p-float-label">
+            <Chips id="chips" v-model="value" />
+            <label for="chips">Chips</label>
+        </span>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const value = ref();
+<\/script>`
+            }
+        };
+    }
+};
+</script>
