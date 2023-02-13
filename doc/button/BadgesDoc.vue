86c09c5db499e4d882fcc6b283855b6b281f1ab1
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-02-14 02:02:56 +0300

    Button docs updated

---
+++ doc/button/BadgesDoc.vue
@@ -0,0 +1,44 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Buttons have built-in badge support with <i>badge</i> and <i>badgeClassName</i> properties.</p>
+    </DocSectionText>
+    <div class="card flex justify-content-center flex-wrap gap-3">
+        <Button type="button" label="Emails" badge="8" />
+        <Button type="button" label="Messages" icon="pi pi-users" class="p-button-warning" badge="8" badgeClass="p-badge-danger" />
+    </div>
+    <DocSectionCode :code="code" v-bind="$attrs" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+<Button type="button" label="Emails" badge="8" />
+<Button type="button" label="Messages" icon="pi pi-users" class="p-button-warning" badge="8" badgeClass="p-badge-danger" />`,
+                options: `
+<template>
+    <div class="card flex justify-content-center flex-wrap gap-3">
+        <Button type="button" label="Emails" badge="8" />
+        <Button type="button" label="Messages" icon="pi pi-users" class="p-button-warning" badge="8" badgeClass="p-badge-danger" />
+    </div>
+</template>
+
+<script>
+<\/script>`,
+                composition: `
+<template>
+    <div class="card flex justify-content-center flex-wrap gap-3">
+        <Button type="button" label="Emails" badge="8" />
+        <Button type="button" label="Messages" icon="pi pi-users" class="p-button-warning" badge="8" badgeClass="p-badge-danger" />
+    </div>
+</template>
+
+<script setup>
+<\/script>`
+            }
+        };
+    }
+};
+</script>
---
+++ doc/button/TemplateDoc.vue
@@ -0,0 +1,49 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Custom content inside a button is defined as children.</p>
+    </DocSectionText>
+    <div class="card flex justify-content-center">
+        <Button class="bg-bluegray-600 hover:bg-bluegray-400 border-bluegray-700">
+            <img alt="logo" src="/images/logo.svg" class="h-2rem" />
+            <div class="ml-2">PrimeVue</div>
+        </Button>
+    </div>
+    <DocSectionCode :code="code" v-bind="$attrs" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+<Button class="bg-bluegray-600 hover:bg-bluegray-400 border-bluegray-700">
+    <img alt="logo" src="/images/logo.svg" class="h-2rem" />
+</Button>`,
+                options: `
+<template>
+    <div class="card flex justify-content-center">
+        <Button class="bg-bluegray-600 hover:bg-bluegray-400 border-bluegray-700">
+            <img alt="logo" src="/images/logo.svg" class="h-2rem" />
+        </Button>
+    </div>
+</template>
+
+<script>
+<\/script>`,
+                composition: `
+<template>
+    <div class="card flex justify-content-center">
+        <Button class="bg-bluegray-600 hover:bg-bluegray-400 border-bluegray-700">
+            <img alt="logo" src="/images/logo.svg" class="h-2rem" />
+        </Button>
+    </div>
+</template>
+
+<script setup>
+<\/script>`
+            }
+        };
+    }
+};
+</script>
