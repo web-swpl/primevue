db16e42d716efd805984924cae706a76830d8e19
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-14 10:57:34 +0300

    refactor

--- layouts/doc/DocSections.vue
+++ layouts/doc/DocSections.vue
@@ -1,13 +1,11 @@
 <template>
-    <template v-for="doc of docs" :key="doc.label">
+    <template v-for="(doc, i) of docs" :key="doc.label + '_' + i">
         <section class="py-3">
             <template v-if="doc.children">
                 <div :id="doc.id">
-                    <h2 :id="doc.id" class="doc-section-label">
-                        {{ doc.label }}
-                        <NuxtLink :id="doc.id" :to="$router.currentRoute.value.path + '#' + doc.id"> # </NuxtLink>
-                    </h2>
-                    <div class="doc-section-description">{{ doc.description || 'Section Content' }}</div>
+                    <DocSectionText :id="doc.id" :label="doc.label">
+                        {{ doc.description || null }}
+                    </DocSectionText>
                 </div>
                 <template v-for="comp of doc.children" :key="comp.label">
                     <component :is="{ ...comp.component }" :id="comp.id" :label="comp.label" :level="2" />
