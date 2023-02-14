76e53b1318295259398f700df20fa1efba9e1577
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-14 10:57:57 +0300

    text layout link fixes

--- layouts/doc/DocSectionText.vue
+++ layouts/doc/DocSectionText.vue
@@ -1,7 +1,7 @@
 <template>
     <component :is="$attrs.level === 2 ? 'h3' : 'h2'" class="doc-section-label">
         {{ $attrs.label }}
-        <NuxtLink :id="$attrs.id" :to="$router.currentRoute.value.path + '#' + $attrs.id" target="_self"> # </NuxtLink>
+        <NuxtLink :id="$attrs.id" :to="`/${$router.currentRoute.value.name}/#${$attrs.id}`" target="_self" @click="onClick"> # </NuxtLink>
     </component>
     <div v-if="$attrs" class="doc-section-description">
         <slot></slot>
@@ -10,6 +10,18 @@
 
 <script>
 export default {
-    inheritAttrs: false
+    inheritAttrs: false,
+    methods: {
+        onClick(event) {
+            const parentElement = event.currentTarget.parentElement;
+            const hash = window.location.hash.substring(1);
+
+            hash === $attrs.id && event.preventDefault();
+
+            setTimeout(() => {
+                parentElement.scrollIntoView({ block: 'start' });
+            }, 0);
+        }
+    }
 };
 </script>
