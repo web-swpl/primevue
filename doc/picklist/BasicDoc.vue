fcfa8c4c816ccd8f3877c8e072c4840d4f713cc8
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-02-14 17:17:36 +0300

    Picklist docs updated

---
+++ doc/picklist/BasicDoc.vue
@@ -0,0 +1,183 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>PickList requires a multidimensional array as its value bound with the v-model directive and a template for its content that gets the item instance and the index via slotProps.</p>
+    </DocSectionText>
+    <div class="card">
+        <PickList v-model="products" listStyle="height:342px" dataKey="id">
+            <template #sourceheader> Available </template>
+            <template #targetheader> Selected </template>
+            <template #item="slotProps">
+                <div class="product-item">
+                    <div class="image-container">
+                        <img :src="'/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                    </div>
+                    <div class="product-list-detail">
+                        <h6 class="mb-2">{{ slotProps.item.name }}</h6>
+                        <i class="pi pi-tag product-category-icon"></i>
+                        <span class="product-category">{{ slotProps.item.category }}</span>
+                    </div>
+                    <div class="product-list-action">
+                        <h6 class="mb-2">${{ slotProps.item.price }}</h6>
+                        <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
+                    </div>
+                </div>
+            </template>
+        </PickList>
+    </div>
+    <DocSectionCode :code="code" v-bind="$attrs" :service="['ProductService']" />
+</template>
+
+<script>
+import { ProductService } from '@/service/ProductService';
+export default {
+    data() {
+        return {
+            products: null,
+            code: {
+                basic: `
+<PickList v-model="products" listStyle="height:342px" dataKey="id">
+    <template #sourceheader> Available </template>
+    <template #targetheader> Selected </template>
+    <template #item="slotProps">
+        <div class="product-item">
+            <div class="image-container">
+                <img :src="'images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+            </div>
+            <div class="product-list-detail">
+                <h6 class="mb-2">{{ slotProps.item.name }}</h6>
+                <i class="pi pi-tag product-category-icon"></i>
+                <span class="product-category">{{ slotProps.item.category }}</span>
+            </div>
+            <div class="product-list-action">
+                <h6 class="mb-2">{{ slotProps.item.price }}</h6>
+                <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
+            </div>
+        </div>
+    </template>
+</PickList>`,
+                options: `
+<template>
+    <div class="card">
+        <PickList v-model="products" listStyle="height:342px" dataKey="id">
+            <template #sourceheader> Available </template>
+            <template #targetheader> Selected </template>
+            <template #item="slotProps">
+                <div class="product-item">
+                    <div class="image-container">
+                        <img :src="'images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                    </div>
+                    <div class="product-list-detail">
+                        <h6 class="mb-2">{{ slotProps.item.name }}</h6>
+                        <i class="pi pi-tag product-category-icon"></i>
+                        <span class="product-category">{{ slotProps.item.category }}</span>
+                    </div>
+                    <div class="product-list-action">
+                        <h6 class="mb-2">{{ slotProps.item.price }}</h6>
+                        <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
+                    </div>
+                </div>
+            </template>
+        </PickList>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            products: null
+        }
+    },
+    mounted() {
+        ProductService.getProductsSmall().then((data) => (this.products = [data, []]));
+    }
+};
+<\/script>`,
+                composition: `
+<template>
+    <div class="card">
+        <PickList v-model="products" listStyle="height:342px" dataKey="id">
+            <template #sourceheader> Available </template>
+            <template #targetheader> Selected </template>
+            <template #item="slotProps">
+                <div class="product-item">
+                    <div class="image-container">
+                        <img :src="'images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                    </div>
+                    <div class="product-list-detail">
+                        <h6 class="mb-2">{{ slotProps.item.name }}</h6>
+                        <i class="pi pi-tag product-category-icon"></i>
+                        <span class="product-category">{{ slotProps.item.category }}</span>
+                    </div>
+                    <div class="product-list-action">
+                        <h6 class="mb-2">{{ slotProps.item.price }}</h6>
+                        <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
+                    </div>
+                </div>
+            </template>
+        </PickList>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const products = ref(null);
+
+onMounted(() => {
+    ProductService.getProductsSmall().then((data) => (products.value = [data, []]));
+});
+<\/script>`
+            }
+        };
+    },
+    mounted() {
+        ProductService.getProductsSmall().then((data) => (this.products = [data, []]));
+    }
+};
+</script>
+
+<style scoped>
+.product-item {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    width: 100%;
+}
+.product-item img {
+    width: 75px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    margin-right: 1rem;
+}
+.product-item .product-list-detail {
+    flex: 1 1 0;
+}
+.product-item .product-list-action {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+}
+.product-item .product-category-icon {
+    vertical-align: middle;
+    margin-right: 0.5rem;
+    font-size: 0.875rem;
+}
+.product-item .product-category {
+    vertical-align: middle;
+    line-height: 1;
+    font-size: 0.875rem;
+}
+@media screen and (max-width: 576px) {
+    .product-item {
+        flex-wrap: wrap;
+    }
+    .product-item .image-container {
+        width: 100%;
+        text-align: center;
+    }
+    .product-item img {
+        margin: 0 0 1rem 0;
+        width: 100px;
+    }
+}
+</style>
