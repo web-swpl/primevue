e3cd7b4592edca04a3440ee069545159c52b8c94
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-02-20 13:25:13 +0300

    refactor

--- doc/paginator/CustomContentDoc.vue
+++ doc/paginator/CustomContentDoc.vue
@@ -20,7 +20,11 @@ export default {
             code: {
                 basic: `
 <Paginator :rows="10" :totalRecords="120" :rowsPerPageOptions="[10, 20, 30]">
-    <template #start="slotProps"> Page: {{ slotProps.state.page }} First: {{ slotProps.state.first }} Rows: {{ slotProps.state.rows }} </template>
+    <template #start="slotProps">
+        Page: {{ slotProps.state.page }}
+        First: {{ slotProps.state.first }}
+        Rows: {{ slotProps.state.rows }}
+    </template>
     <template #end>
         <Button type="button" icon="pi pi-search" />
     </template>
@@ -29,7 +33,11 @@ export default {
 <template>
     <div class="card">
         <Paginator :rows="10" :totalRecords="120" :rowsPerPageOptions="[10, 20, 30]">
-            <template #start="slotProps"> Page: {{ slotProps.state.page }} First: {{ slotProps.state.first }} Rows: {{ slotProps.state.rows }} </template>
+            <template #start="slotProps">
+                Page: {{ slotProps.state.page }}
+                First: {{ slotProps.state.first }}
+                Rows: {{ slotProps.state.rows }}
+            </template>
             <template #end>
                 <Button type="button" icon="pi pi-search" />
             </template>
@@ -43,7 +51,11 @@ export default {
 <template>
     <div class="card">
         <Paginator :rows="10" :totalRecords="120" :rowsPerPageOptions="[10, 20, 30]">
-            <template #start="slotProps"> Page: {{ slotProps.state.page }} First: {{ slotProps.state.first }} Rows: {{ slotProps.state.rows }} </template>
+            <template #start="slotProps">
+                Page: {{ slotProps.state.page }}
+                First: {{ slotProps.state.first }}
+                Rows: {{ slotProps.state.rows }}
+            </template>
             <template #end>
                 <Button type="button" icon="pi pi-search" />
             </template>
