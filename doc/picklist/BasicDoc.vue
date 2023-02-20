7cdb60c72542bdf0973fb84e675f46b9efa7fe45
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-02-20 12:06:36 +0300

    OrderList Picklist styles removed

--- doc/picklist/BasicDoc.vue
+++ doc/picklist/BasicDoc.vue
@@ -7,19 +7,16 @@
             <template #sourceheader> Available </template>
             <template #targetheader> Selected </template>
             <template #item="slotProps">
-                <div class="product-item">
-                    <div class="image-container">
-                        <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                <div class="flex flex-wrap p-2 align-items-center gap-3">
+                    <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                    <div class="flex-1 flex flex-column gap-2">
+                        <span class="font-bold">{{ slotProps.item.name }}</span>
+                        <div class="flex align-items-center gap-2">
+                            <i class="pi pi-tag text-sm"></i>
+                            <span>{{ slotProps.item.category }}</span>
+                        </div>
                     </div>
-                    <div class="product-list-detail">
-                        <h6 class="mb-2">{{ slotProps.item.name }}</h6>
-                        <i class="pi pi-tag product-category-icon"></i>
-                        <span class="product-category">{{ slotProps.item.category }}</span>
-                    </div>
-                    <div class="product-list-action">
-                        <h6 class="mb-2">${{ slotProps.item.price }}</h6>
-                        <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
-                    </div>
+                    <span class="font-bold text-900">${{ slotProps.item.price }}</span>
                 </div>
             </template>
         </PickList>
@@ -40,19 +37,16 @@ export default {
     <template #sourceheader> Available </template>
     <template #targetheader> Selected </template>
     <template #item="slotProps">
-        <div class="product-item">
-            <div class="image-container">
-                <img :src="'/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+        <div class="flex flex-wrap p-2 align-items-center gap-3">
+            <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+            <div class="flex-1 flex flex-column gap-2">
+                <span class="font-bold">{{ slotProps.item.name }}</span>
+                <div class="flex align-items-center gap-2">
+                    <i class="pi pi-tag text-sm"></i>
+                    <span>{{ slotProps.item.category }}</span>
+                </div>
             </div>
-            <div class="product-list-detail">
-                <h6 class="mb-2">{{ slotProps.item.name }}</h6>
-                <i class="pi pi-tag product-category-icon"></i>
-                <span class="product-category">{{ slotProps.item.category }}</span>
-            </div>
-            <div class="product-list-action">
-                <h6 class="mb-2">{{ slotProps.item.price }}</h6>
-                <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
-            </div>
+            <span class="font-bold text-900">$ {{ slotProps.item.price }}</span>
         </div>
     </template>
 </PickList>`,
@@ -63,19 +57,16 @@ export default {
             <template #sourceheader> Available </template>
             <template #targetheader> Selected </template>
             <template #item="slotProps">
-                <div class="product-item">
-                    <div class="image-container">
-                        <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                <div class="flex flex-wrap p-2 align-items-center gap-3">
+                    <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                    <div class="flex-1 flex flex-column gap-2">
+                        <span class="font-bold">{{ slotProps.item.name }}</span>
+                        <div class="flex align-items-center gap-2">
+                            <i class="pi pi-tag text-sm"></i>
+                            <span>{{ slotProps.item.category }}</span>
+                        </div>
                     </div>
-                    <div class="product-list-detail">
-                        <h6 class="mb-2">{{ slotProps.item.name }}</h6>
-                        <i class="pi pi-tag product-category-icon"></i>
-                        <span class="product-category">{{ slotProps.item.category }}</span>
-                    </div>
-                    <div class="product-list-action">
-                        <h6 class="mb-2">{{ slotProps.item.price }}</h6>
-                        <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
-                    </div>
+                    <span class="font-bold text-900">$ {{ slotProps.item.price }}</span>
                 </div>
             </template>
         </PickList>
@@ -94,52 +85,7 @@ export default {
         ProductService.getProductsSmall().then((data) => (this.products = [data, []]));
     }
 };
-<\/script>
-
-<style scoped>
-.product-item {
-    display: flex;
-    align-items: center;
-    padding: 0.5rem;
-    width: 100%;
-}
-.product-item img {
-    width: 75px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    margin-right: 1rem;
-}
-.product-item .product-list-detail {
-    flex: 1 1 0;
-}
-.product-item .product-list-action {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-}
-.product-item .product-category-icon {
-    vertical-align: middle;
-    margin-right: 0.5rem;
-    font-size: 0.875rem;
-}
-.product-item .product-category {
-    vertical-align: middle;
-    line-height: 1;
-    font-size: 0.875rem;
-}
-@media screen and (max-width: 576px) {
-    .product-item {
-        flex-wrap: wrap;
-    }
-    .product-item .image-container {
-        width: 100%;
-        text-align: center;
-    }
-    .product-item img {
-        margin: 0 0 1rem 0;
-        width: 100px;
-    }
-}
-</style>`,
+<\/script>`,
                 composition: `
 <template>
     <div class="card">
@@ -147,19 +93,16 @@ export default {
             <template #sourceheader> Available </template>
             <template #targetheader> Selected </template>
             <template #item="slotProps">
-                <div class="product-item">
-                    <div class="image-container">
-                        <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                <div class="flex flex-wrap p-2 align-items-center gap-3">
+                    <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.item.image" :alt="slotProps.item.name" />
+                    <div class="flex-1 flex flex-column gap-2">
+                        <span class="font-bold">{{ slotProps.item.name }}</span>
+                        <div class="flex align-items-center gap-2">
+                            <i class="pi pi-tag text-sm"></i>
+                            <span>{{ slotProps.item.category }}</span>
+                        </div>
                     </div>
-                    <div class="product-list-detail">
-                        <h6 class="mb-2">{{ slotProps.item.name }}</h6>
-                        <i class="pi pi-tag product-category-icon"></i>
-                        <span class="product-category">{{ slotProps.item.category }}</span>
-                    </div>
-                    <div class="product-list-action">
-                        <h6 class="mb-2">{{ slotProps.item.price }}</h6>
-                        <span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
-                    </div>
+                    <span class="font-bold text-900">$ {{ slotProps.item.price }}</span>
                 </div>
             </template>
         </PickList>
@@ -175,52 +118,23 @@ const products = ref(null);
 onMounted(() => {
     ProductService.getProductsSmall().then((data) => (products.value = [data, []]));
 });
-<\/script>
-
-<style scoped>
-.product-item {
-    display: flex;
-    align-items: center;
-    padding: 0.5rem;
-    width: 100%;
-}
-.product-item img {
-    width: 75px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    margin-right: 1rem;
-}
-.product-item .product-list-detail {
-    flex: 1 1 0;
-}
-.product-item .product-list-action {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-}
-.product-item .product-category-icon {
-    vertical-align: middle;
-    margin-right: 0.5rem;
-    font-size: 0.875rem;
-}
-.product-item .product-category {
-    vertical-align: middle;
-    line-height: 1;
-    font-size: 0.875rem;
-}
-@media screen and (max-width: 576px) {
-    .product-item {
-        flex-wrap: wrap;
-    }
-    .product-item .image-container {
-        width: 100%;
-        text-align: center;
-    }
-    .product-item img {
-        margin: 0 0 1rem 0;
-        width: 100px;
-    }
-}
-</style>`
+<\/script>`,
+                data: `
+/* ProductService */
+{
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: '/bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+...
+        `
             }
         };
     },
@@ -229,48 +143,3 @@ onMounted(() => {
     }
 };
 </script>
-
-<style scoped>
-.product-item {
-    display: flex;
-    align-items: center;
-    padding: 0.5rem;
-    width: 100%;
-}
-.product-item img {
-    width: 75px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    margin-right: 1rem;
-}
-.product-item .product-list-detail {
-    flex: 1 1 0;
-}
-.product-item .product-list-action {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-}
-.product-item .product-category-icon {
-    vertical-align: middle;
-    margin-right: 0.5rem;
-    font-size: 0.875rem;
-}
-.product-item .product-category {
-    vertical-align: middle;
-    line-height: 1;
-    font-size: 0.875rem;
-}
-@media screen and (max-width: 576px) {
-    .product-item {
-        flex-wrap: wrap;
-    }
-    .product-item .image-container {
-        width: 100%;
-        text-align: center;
-    }
-    .product-item img {
-        margin: 0 0 1rem 0;
-        width: 100px;
-    }
-}
-</style>
